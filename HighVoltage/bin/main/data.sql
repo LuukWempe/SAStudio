dcf1781d636aee34e75e06148b41e5defb0a4679
Luuk Wempe <luukwempe@hotmail.com>
2021-12-02 15:38:53 +0100

    Table created in Signal Overview. Signals can be shown in the webpage

--- HighVoltage/bin/main/data.sql
+++ HighVoltage/bin/main/data.sql
@@ -15,7 +15,8 @@ insert into IED values(8,'10.47.97.14','IO-Box',1);
 insert into IED values(9,'10.47.97.16','Switch 1',1);
 insert into IED values(10,'10.47.97.17','Switch 2',1);
 
---insert into IEDSIGNALS values(1,165216842,5,'Communication Enabled');
+insert into IEDSIGNALS values(1,'SW','DI5',1,2,'DI3',3,4,'-',0,0,'-',0,0,'I',0,'X14',0,2,1,'2s',1,'Hulpspanning Motor',5);
+insert into IEDSIGNALS values(2,'SW','DI6',1,2,'DI4',2,5,'-',0,0,'-',0,0,'J',0,'X11',0,3,4,'10s',1,'VS Veer',5);
 --insert into IEDSIGNALS values(2,152182442,5,'Communication Error');
 --insert into IEDSIGNALS values(3,364896126,5,'Connection Esablished');
 --insert into IEDSIGNALS values(4,932571568,5,'No Short Circuit');
--- HighVoltage/src/main/java/com/stedin/HighVoltage/model/IEDSignal.java
+++ HighVoltage/src/main/java/com/stedin/HighVoltage/model/IEDSignal.java
@@ -124,13 +124,77 @@ public class IEDSignal{
 	//Getters and Setters
 	public Long getSignalId() {return signalId;}
 	public void setSignalId(Long signalId) {this.signalId = signalId;}
-	
-	public String getSignalName() {return name;}
-	public void setSignalName(String name) {this.name = name;}
-	
+
 	public IED getIed() {return ied;}
-	public void setIED(IED ied) {this.ied = ied;}
+	public void setIed(IED ied) {this.ied = ied;}
+
+	public String getName() {return name;}
+	public void setName(String name) {this.name = name;}
+
+	public boolean getFp_inStation() {return fp_inStation;}
+	public void setFp_inStation(boolean fp_inStation) {this.fp_inStation = fp_inStation;}
+
+	public int getFp_NumDiAi() {return fp_NumDiAi;}
+	public void setFp_NumDiAi(int fp_NumDiAi) {this.fp_NumDiAi = fp_NumDiAi;}
+
+	public int getFp_NumDu() {return fp_NumDu;}
+	public void setFp_NumDu(int fp_NumDu) {this.fp_NumDu = fp_NumDu;}
+
+	public String getFp_Vertraging() {return fp_Vertraging;}
+	public void setFp_Vertraging(String fp_Vertraging) {this.fp_Vertraging = fp_Vertraging;}
+
+	public String getFp_HwSw() {return fp_HwSw;}
+	public void setFp_HwSw(String fp_HwSw) {this.fp_HwSw = fp_HwSw;}
+
+	public String getFp_IedSlot() {return fp_IedSlot;}
+	public void setFp_IedSlot(String fp_IedSlot) {this.fp_IedSlot = fp_IedSlot;}
+
+	public String getFp_Klem() {return fp_Klem;}
+	public void setFp_Klem(String fp_Klem) {this.fp_Klem = fp_Klem;}
+
+	public String getFp_IedIO1() {return fp_IedIO1;}
+	public void setFp_IedIO1(String fp_IedIO1) {this.fp_IedIO1 = fp_IedIO1;}
+
+	public int getFp_IedIO1Pos() {return fp_IedIO1Pos;}
+	public void setFp_IedIO1Pos(int fp_IedIO1Pos) {this.fp_IedIO1Pos = fp_IedIO1Pos;}
+
+	public int getFp_IedIO1Neg() {return fp_IedIO1Neg;}
+	public void setFp_IedIO1Neg(int fp_IedIO1Neg) {this.fp_IedIO1Neg = fp_IedIO1Neg;}
+
+	public String getFp_IedIO2() {return fp_IedIO2;}
+	public void setFp_IedIO2(String fp_IedIO2) {this.fp_IedIO2 = fp_IedIO2;}
+
+	public int getFp_IedIO2Pos() {return fp_IedIO2Pos;}
+	public void setFp_IedIO2Pos(int fp_IedIO2Pos) {this.fp_IedIO2Pos = fp_IedIO2Pos;}
+
+	public int getFp_IedIO2Neg() {return fp_IedIO2Neg;}
+	public void setFp_IedIO2Neg(int fp_IedIO2Neg) {this.fp_IedIO2Neg = fp_IedIO2Neg;}
+
+	public String getFp_IedIO3() {return fp_IedIO3;}
+	public void setFp_IedIO3(String fp_IedIO3) {this.fp_IedIO3 = fp_IedIO3;}
+
+	public int getFp_IedIO3Pos() {return fp_IedIO3Pos;}
+	public void setFp_IedIO3Pos(int fp_IedIO3Pos) {this.fp_IedIO3Pos = fp_IedIO3Pos;}
+
+	public int getFp_IedIO3Neg() {return fp_IedIO3Neg;}
+	public void setFp_IedIO3Neg(int fp_IedIO3Neg) {this.fp_IedIO3Neg = fp_IedIO3Neg;}
+
+	public String getFp_IedIO4() {return fp_IedIO4;}
+	public void setFp_IedIO4(String fp_IedIO4) {this.fp_IedIO4 = fp_IedIO4;}
+
+	public int getFp_IedIO4Pos() {return fp_IedIO4Pos;}
+	public void setFp_IedIO4Pos(int fp_IedIO4Pos) {this.fp_IedIO4Pos = fp_IedIO4Pos;}
+
+	public int getFp_IedIO4Neg() {return fp_IedIO4Neg;}
+	public void setFp_IedIO4Neg(int fp_IedIO4Neg) {this.fp_IedIO4Neg = fp_IedIO4Neg;}
+
+	public boolean getFp_NormalePositie() {return fp_NormalePositie;}
+	public void setFp_NormalePositie(boolean fp_NormalePositie) {this.fp_NormalePositie = fp_NormalePositie;}
 	
+	public boolean getFp_Inverteren() {return fp_Inverteren;}
+	public void setFp_Inverteren(boolean fp_Inverteren) {this.fp_Inverteren = fp_Inverteren;}
+
+
 	//Methods
 	public Long getIedId() {
 		return this.ied.getIedId();
